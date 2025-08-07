5085751fdb504edf883221d687316c6faaeb53da
Jonathan Maingre <jonathan.maingre@outlook.com>
2025-08-07 15:43:58 +0200

    Improve bot

--- bot.js
+++ bot.js
@@ -3,14 +3,16 @@ const axios = require('axios');
 
 const webhookUrl = 'https://script.google.com/macros/s/AKfycbzAszYfxvjeazZ1hE1ax0Ks_VUjONUmBM66RS60PEh64fn9EFjKsqsP9Xo__V4Fozs/exec';
 
-const accessToken = '-';
-const clientId = '-';
+const clientId = 'l0yhmcda38qvxv1ht2tlkt7fdpdrz4';
+const accessToken = 'mt2ttz80uo1jldpthwna43h6vrecpi';
+
+
 
 const client = new tmi.Client({
     options: { debug: true },
     identity: {
         username: 'MyLovelyBot',
-        password: '-'
+        password: 'k7z0ao0br66ltgwr1l4cnotuzuqotz' // à générer ici : https://twitchapps.com/tmi/
     },
     channels: [ 'rocknrollamlp', 'MyLovelyPlanet' ]
 });
@@ -22,7 +24,7 @@ client.on('message', async (channel, tags, message, self) => {
 
     message = message.trimEnd();
     message = message.trimStart();
-    
+
     if (message.startsWith('!father')) {
         const parts = message.split(' ');
         if (parts.length < 2 || !parts[1].startsWith('@')) {
@@ -32,9 +34,9 @@ client.on('message', async (channel, tags, message, self) => {
 
         const father = parts[1].slice(1);
         const username = tags['display-name'];
-        
+
         const exists = await checkIfTwitchUserExists(father);
-        
+
         if (!exists) {
             client.say(channel, `❌ Le compte Twitch @${father} n'existe pas.`);
             return;
